7bfd3d9fc7956b9d637ac4dfd42d30b3566c28fd
Sergei Tikhomirov <sergey.zr.co.uk@gmail.com>
2024-05-08 02:06:49 +0300

    Edited sample (#6)
    * Edited sample

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='bioage_framework',
     packages=find_packages(include=['bioage_framework']),
-    version='0.1.0',
+    version='0.1.1',
     description='Library to merge BioAge models with LLMs',
     author='Sergei Tikhomirov',
-    install_requires=['requests', 'numpy', 'pandas', 'torch>=1.10.0', 'pytorch-lightning==1.6.4', 'scipy==1.10.1', 'omegaconf', 'scikit-learn>=1.1.3', 'shap'],
+    install_requires=['requests', 'numpy', 'pandas', 'torch', 'pytorch-lightning', 'scipy==1.10.1', 
+                      'omegaconf', 'scikit-learn', 'shap', 'pytorch-widedeep==1.1.1'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==8.2.0'],
     test_suite='tests',
