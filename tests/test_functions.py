225486c277b1c7c70cbf39c2f3ee086087a63f81
Sergei Tikhomirov <sergey.zr.co.uk@gmail.com>
2024-07-17 15:29:10 +0300

    fixed test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,9 +1,8 @@
-from llamba import chat_model
-from llamba import connector
+from llamba.chat_model import ChatbaseModel
 
 # Test for local machine only, since API keys can't be transferred to github safely
 # Possible workaround -- self-hosted test runner
 def test_query():
-    chatbot = connector.ChatModel(url="<URL>", api_key="AAAA", chatbot_id="BBBB")
+    chatbot = ChatbaseModel(url="<URL>", api_key="AAAA", chatbot_id="BBBB")
     res = chatbot.query('What does an increased level of albumin mean?')
     assert res[0] == False
