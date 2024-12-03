ebb3fcfadf1ba2f3754860d7566fdc65b2ebad0b
Sergei Tikhomirov <s.tikhomirov@arenadata.io>
2024-12-03 14:16:29 +0300

    improved code

--- llamba/connector.py
+++ llamba/connector.py
@@ -7,6 +7,7 @@ import numpy as np
 from llamba.chatmodels.chat_model import AbstractChatModel
 from llamba_library.bioage_model import BioAgeModel
 from llamba_library.plots import kde_plot
+from llamba_library.functions import get_top_shap
 
 class LlambaConnector:
     def __init__(self, bioage_model: BioAgeModel, chat_model: AbstractChatModel):
@@ -30,39 +31,23 @@ class LlambaConnector:
             self.answer += 'you are ageing slower than normal.\n\n'
 
         shap_dict = kwargs.get('shap_dict', None)
-
-        if shap_dict == None:
+        top_n = kwargs.get('top_n', None)
+        if shap_dict == None or top_n == None:
             return {"analysis": self.answer, "acceleration": acceleration[0]}
         
         self.answer += 'Here is some more information about your data. \n\n'
         
         explainer = shap_dict['explainer']
-
         feats = data.drop(['Age', 'bio_age'], axis=1).columns.to_list()
+        sorted_values, sorted_data, sorted_feats = get_top_shap(top_n, data, feats, explainer)
         
-        self.generate_prompts(n=2, data=data, feats=feats, explainer=explainer)
+        self.generate_prompts(top_n=top_n, data=sorted_data, feats=sorted_feats, values=sorted_values)
         self.query_prompts()
 
         return {"analysis": self.answer, "acceleration": acceleration[0], "features": feats}    
 
-    def generate_prompts(self, data, n, feats, explainer):
-        shap_values_trgt = explainer.shap_values(data.loc[0, feats].values)
-        base_value = explainer.expected_value[0]
-
-        explanation = shap.Explanation(
-            values=shap_values_trgt,
-            base_values=base_value,
-            data=data.loc[0, feats].values,
-            feature_names=feats)
-
-        permutation = np.array(explanation.values).argsort()
-        
-        # Top-n values
-        values = np.flip(np.array(explanation.values)[permutation])[:n]
-        data = np.flip(np.array(explanation.data)[permutation])[:n]
-        feats = np.flip(np.array(feats)[permutation])[:n]
-
-        for i in range(n):
+    def generate_prompts(self, top_n, data, feats, values):
+        for i in range(top_n):
             self.answer += f'{feats[i]}: {data[i]}\n'
             if values[i] > 0:
                 level = 'an increased'
